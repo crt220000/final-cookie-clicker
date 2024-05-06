aef29b3f04f8ef0df34f05b66f1ea7c1960712cf
Catalina Thompson <crt220000@utdallas.edu>
2024-05-05 20:35:46 -0500

    added clicker counter

--- src/project.py
+++ src/project.py
@@ -7,6 +7,11 @@ HEIGHT = 720
 def draw(screen, cookie_img, cookie_rect):
     screen.blit(cookie_img, cookie_rect)
 
+def counter(screen, click_count):
+    font = pygame.font.SysFont("Comic Sans MS", 50)
+    text = font.render(f"Clicks: {click_count[0]}", True, (19, 8, 74))
+    screen.blit(text, (370,530))
+
 def main():
     pygame.init()
     window_res = (WIDTH, HEIGHT)
@@ -14,6 +19,7 @@ def main():
     pygame.display.set_caption("Cookie Clicker")
     cookie_img = pygame.image.load('cookie.png')
     cookie_rect = cookie_img.get_rect(center=(WIDTH//2, HEIGHT//2))
+    click_count = [0]
     running = True
     while running:
         for event in pygame.event.get():
@@ -22,6 +28,7 @@ def main():
        
         screen.fill((184, 153, 204))
         draw(screen, cookie_img, cookie_rect)
+        counter(screen, click_count)
         pygame.display.flip()
     pygame.quit()
 
