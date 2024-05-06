d48f96de39484647871ef07935d629903fcd75e5
Catalina Thompson <crt220000@utdallas.edu>
2024-05-05 20:39:53 -0500

    added sound fx for cookie being clicked

--- src/project.py
+++ src/project.py
@@ -12,7 +12,7 @@ def counter(screen, click_count):
     text = font.render(f"Clicks: {click_count[0]}", True, (19, 8, 74))
     screen.blit(text, (370,530))
 
-def user_interaction(cookie_rect, cookie_img, cookie_clicked_img, screen, click_count):
+def user_interaction(cookie_rect, cookie_img, cookie_clicked_img, screen, click_count, crunch):
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
@@ -24,6 +24,7 @@ def user_interaction(cookie_rect, cookie_img, cookie_clicked_img, screen, click_
                     pygame.time.delay(100)
                     screen.blit(cookie_img, cookie_rect)
                     pygame.display.flip()
+                    crunch.play()
                     print("Cookie clicked!")
 
 def main():
@@ -35,6 +36,7 @@ def main():
     cookie_clicked_img = pygame.image.load('cookie-clicked.png')
     cookie_rect = cookie_img.get_rect(center=(WIDTH//2, HEIGHT//2))
     click_count = [0]
+    crunch = pygame.mixer.Sound('crunch.wav')
     running = True
     while running:
         for event in pygame.event.get():
@@ -44,7 +46,7 @@ def main():
         screen.fill((184, 153, 204))
         draw(screen, cookie_img, cookie_rect)
         counter(screen, click_count)
-        user_interaction(cookie_rect, cookie_img, cookie_clicked_img, screen, click_count)
+        user_interaction(cookie_rect, cookie_img, cookie_clicked_img, screen, click_count, crunch)
         pygame.display.flip()
     pygame.quit()
 
